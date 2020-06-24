2e37339f889aed0862a97ac38bf0ac60145004ee
Farzaneh Darki <farzaneh.darki@gmail.com>
2020-06-24 13:18:04 +0100

    Update Fig3C_traditional_ode23tb.m

--- Fig3C_traditional_ode23tb.m
+++ Fig3C_traditional_ode23tb.m
@@ -1,13 +1,13 @@
+clear; close all;
 %% parameters
-h=1;      %adaptation strength; WTA:(h=1), RIV(h=4.3); SIM(h=15)
-g=1.5;     %inhibition strength
-tau=20;    %ms
-tau_H=900; %ms
-tau_I=11;  %ms
+h=1;           %adaptation strength; WTA:(h=1), RIV(h=4.3); SIM(h=15)
+g=1.5;         %inhibition strength
+tau=20;        %ms
+tau_H=900;     %ms
+tau_I=11;      %ms
 t_total=20000; %simulation time in [ms]
-v=10;   
-J_HL=v;    %inputs to populations representing horizontal grating in the left eye
-J_VR=v;    %inputs to populations representing vertical grating in the right eye
+J_HL=10;       %inputs to populations representing horizontal grating in the left eye
+J_VR=10;       %inputs to populations representing vertical grating in the right eye
 %% Wilson model 
 Wilson=@(t,u)[...
     (1/tau)*(-u(1,:)+(100*((J_HL-g*u(6,:))>0)*(J_HL-g*u(6,:))^2)/((10+u(2,:))^2+((J_HL-g*u(6,:))>0)*(J_HL-g*u(6,:))^2));
@@ -17,16 +17,16 @@ Wilson=@(t,u)[...
     (1/tau_H)*(-u(5,:)+h*u(4,:));
     (1/tau_I)*(-u(6,:)+u(4,:))];
 %% Solve ODEs with ode23tb
-x0=zeros(6,1); x0(1,1)=0.000001; %initialize conditions
+x0=zeros(6,1); x0(1,1)=0.000001; %initial conditions
 tspan=[0 t_total];
 options = odeset('AbsTol',1e-7,'RelTol',1e-7);
 [t,xt] = ode23tb(Wilson, tspan, x0, options);
 t=t/1000; % convert to [s] from [ms]
 %% Plot time histories
 figure
-plot(t,xt(:,1),'b','linewidth',2)
-title(['V1=V2=',num2str(v),',  g=',num2str(g),',  h=',num2str(h)])
+plot(t,xt(:,1),'b')
 hold on
-plot(t,xt(:,4),'b   :','linewidth',2)
-ylabel('E_1 & E_2')
+plot(t,xt(:,4),'b--')
+title(['Traditional case,  J_{HL}=J_{VR}=',num2str(J_HL),',  g=',num2str(g),',  h=',num2str(h)])
+ylabel('E_1 or E_2')
 xlabel('Time [Sec]')
